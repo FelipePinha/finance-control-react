5c768271acef5738427a6f879b3eddf698c431b5
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-26 15:55:05 -0300

    chore: create finances route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Finances } from './pages/Finances';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home />,
+    },
+    {
+        path: '/finanças',
+        element: <Finances />,
     },
 ]);
 
