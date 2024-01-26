51296c24850faf0c75f34959a45bedab45a5c9a9
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-26 13:50:49 -0300

    chore: config routes

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <div className="bg-blue-500">
-            <h1 className="text-white">É O FINANÇAS SHAUHSAUSHAU</h1>
-        </div>
-    );
-}
-
-export default App;
