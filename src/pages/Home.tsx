e6ef8680c27bbd3438ee0a9b5b8d66855ef9f08e
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-26 15:09:35 -0300

    feat: create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,32 @@
+import { Link } from 'react-router-dom';
+import HomeBanner from '../images/home-banner.jpg';
+
 export const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className="w-screen h-screen bg-zinc-800 flex justify-center items-center">
+            <section className="flex w-full h-full mx-auto justify-center items-center gap-10">
+                <div
+                    className="w-[60%] h-full hidden md:block bg-no-repeat bg-cover"
+                    style={{ backgroundImage: `url(${HomeBanner})` }}
+                >
+                    {/* <img src={HomeBanner} alt="banner" /> */}
+                </div>
+                <div className="w-full md:w-[40%] flex flex-col gap-3 p-6 items-start justify-center">
+                    <p className="text-white font-bold">
+                        <span className="text-pink-800">Nu</span> Kenzie
+                    </p>
+                    <h1 className="text-white text-3xl font-bold">
+                        Centralize o controle de suas finanças
+                    </h1>
+                    <p className="text-white">de forma rápida e segura</p>
+                    <Link
+                        className="text-white font-bold bg-pink-800 rounded-md w-full text-center p-3 hover:bg-pink-600 duration-200 shadow-sm"
+                        to={'#'}
+                    >
+                        Iniciar
+                    </Link>
+                </div>
+            </section>
+        </div>
+    );
 };
