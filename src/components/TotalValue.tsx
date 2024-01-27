6b7b63da81b58659e3c57c5195d937f5c750f06b
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-26 21:59:02 -0300

    feat: create total value component

---
+++ src/components/TotalValue.tsx
@@ -0,0 +1,11 @@
+export const TotalValue = () => {
+    return (
+        <article className="bg-white shadow-md p-3 rounded-sm">
+            <div className="flex justify-between items-center mb-5">
+                <h2 className="text-lg font-bold">Valor total:</h2>
+                <p className="text-pink-800 font-bold">$ 0</p>
+            </div>
+            <p>O valor se refere ao saldo.</p>
+        </article>
+    );
+};
