e3aa3b1a4569bfbee844f68a698562610a7f2235
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-26 22:00:06 -0300

    feat: create total value component

--- src/pages/Finances.tsx
+++ src/pages/Finances.tsx
@@ -1,13 +1,15 @@
 import { Form } from '../components/Form';
 import { Header } from '../components/Header';
+import { TotalValue } from '../components/TotalValue';
 
 export const Finances = () => {
     return (
         <div className="w-screen h-screen bg-zinc-100">
             <Header />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-10 md:px-52 mt-14">
-                <section>
+                <section className="grid gap-4">
                     <Form />
+                    <TotalValue />
                 </section>
                 <section>
                     <h1>Lista de finanças</h1>
