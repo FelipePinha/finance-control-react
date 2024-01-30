9b243b54a0e451047e568c7de78a6e5193110e17
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-30 10:31:23 -0300

    feat: create 'sum all values' function

--- src/components/TotalValue.tsx
+++ src/components/TotalValue.tsx
@@ -1,9 +1,33 @@
-export const TotalValue = () => {
+import { Finance } from '../types/finances-types';
+
+interface TotalValueProps {
+    finances: Finance[];
+}
+
+export const TotalValue = ({ finances }: TotalValueProps) => {
+    const sumAllValues = () => {
+        let sum = 0;
+
+        if (finances.length === 0) {
+            return 0;
+        } else {
+            for (let i = 0; i < finances.length; i++) {
+                if (finances[i].valueType === 'expense') {
+                    sum = sum - Number(finances[i].value);
+                } else {
+                    sum = sum + Number(finances[i].value);
+                }
+            }
+        }
+
+        return sum.toFixed(2);
+    };
+
     return (
         <article className="bg-white shadow-md p-3 rounded-sm">
             <div className="flex justify-between items-center mb-5">
                 <h2 className="text-lg font-bold">Valor total:</h2>
-                <p className="text-pink-800 font-bold">$ 0</p>
+                <p className="text-pink-800 font-bold">$ {sumAllValues()}</p>
             </div>
             <p>O valor se refere ao saldo.</p>
         </article>
--- src/pages/Finances.tsx
+++ src/pages/Finances.tsx
@@ -33,7 +33,7 @@ export const Finances = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-10 md:px-52 mt-14 max-h-0">
                 <section className="grid gap-4 h-fit">
                     <Form />
-                    <TotalValue />
+                    <TotalValue finances={finances} />
                 </section>
                 <section>
                     <Summary filter={filter} setFilter={setFilter} />
